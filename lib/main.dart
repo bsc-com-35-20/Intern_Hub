fbb0707ca4b46cda91167ae7c8ea73d0ccbb8f0b
bsc-com-35-20 <bsc-com-35-20@unima.ac.mw>
2024-10-20 07:09:20 +0200

    Trying to create a user profile updating page

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart'; // Import Firebase core
 import 'package:internhub/LogIn_ And_Register/Log_In.dart';
 import 'package:internhub/LogIn_ And_Register/Register.dart';
+import 'package:internhub/Settings/UpdateProfile.dart';
 import 'firebase_options.dart';
 import 'package:internhub/Home/HomePage.dart';
 import 'package:internhub/Home/Vacancies.dart';
 import 'package:internhub/Vacancy/VacancyDetails1.dart';
 import 'package:internhub/Vacancy/VacancyDetails2.dart';
 import 'package:internhub/Vacancy/VacancyDetails3.dart';
+
 
 
 void main() async {
@@ -38,6 +40,7 @@ class MyApp extends StatelessWidget {
         '/LogIn': (context) => Log_In(), // Define LogIn route
         '/Register': (context) => Register(), // Define Register route
         '/Home': (context) => HomePage(),
+        '/UpdateProfile':(context) => UpdateProfile(),
 
         '/VacancyDetails1': (context) => VacancyDetails1(),
         '/VacancyDetails2': (context) => VacancyDetails2(),
